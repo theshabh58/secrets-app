d78a269d0383d5e103803834e66a50b527163766
Rishabh <theshabh58@gmail.com>
2020-11-07 11:16:46 -0600

    add env var for app name

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets",
+  callbackURL: process.env.APP_NAME + "/auth/google/secrets",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 }, function(accessToken, refreshToken, profile, cb) {
   console.log(profile);
@@ -78,7 +78,7 @@ passport.use(new GoogleStrategy({
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GIYHUN_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/secrets",
+  callbackURL: process.env.APP_NAME + "/auth/github/secrets",
 }, function(accessToken, refreshToken, profile, cb) {
   console.log(profile);
   User.findOrCreate({
