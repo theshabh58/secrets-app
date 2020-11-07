039b1e8b19ce4e353bc90b9f7c88dc8aad7d0682
Rishabh <theshabh58@gmail.com>
2020-11-07 11:04:58 -0600

    updated app port

--- app.js
+++ app.js
@@ -209,6 +209,6 @@ app.post("/submit", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("Server started on port: 3000");
 });
